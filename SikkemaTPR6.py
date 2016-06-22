4bc474cc80566e3a0f055f71a2e5333e0291ef5d
tylertks <tylertks@gmail.com>
2016-06-22 14:48:02 -0400

    CITP110 Assnmt6

---
+++ SikkemaTPR6.py
@@ -0,0 +1,22 @@
+#SikkemaTPR6.py
+#Tyler Sikkema
+#June 22, 2016
+#Abstract: Rock, Paper, Scissors game
+#============================================
+
+def Main():
+    #Main function
+    #Calls other functions
+    #at end, display win/lose/draw
+
+def GetInput():
+    #Get and validate input
+
+def ComputerInput():
+    #Randomly generate a choice for the computer player
+
+def GetWinner():
+    #take the player and computer input
+    #determine a winner
+    #return the winner based on input
+    
